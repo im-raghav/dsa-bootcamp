fd9a7afdee30b91be083de33b2c3ffa981c015b1
Himanshu Raghav <itsmeraghuvanshi@gmail.com>
2023-02-03 12:35:26 +0530

    Q.armstrong number

---
+++ adhoc/Armstrong.java
@@ -0,0 +1,25 @@
+// prints first 1000 armstrong number 
+
+package adhoc;
+
+public class Armstrong {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 1000; i++) {
+            if (isArmstrong(i))
+                System.out.println(i);
+        }
+    }
+
+    static boolean isArmstrong(int num) {
+        int sum = 0;
+        int originalNum = num;
+
+        while (num != 0) {
+            int digit = num % 10;
+            sum += digit * digit * digit;
+            num /= 10;
+        }
+        return (sum == originalNum);
+    }
+}
