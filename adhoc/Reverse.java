fd0f1d5d0c0ea04217689863b0cd292e0b3927e4
Himanshu Raghav <itsmeraghuvanshi@gmail.com>
2023-02-03 10:08:13 +0530

    Q.reverse of a number

---
+++ adhoc/Reverse.java
@@ -0,0 +1,27 @@
+// reverse the given number  
+package adhoc;
+
+import java.util.Scanner;
+
+import javax.swing.SortOrder;
+
+public class Reverse {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number to be reversed: ");
+        int num = input.nextInt();
+
+        int reversedNum = 0;
+
+        while (num != 0) {
+            
+            int temp = num % 10;
+            reversedNum = reversedNum * 10 + temp;
+            num /= 10;
+        }
+        System.out.printf("The reverse number is %d", reversedNum);
+        input.close();
+    }
+}
+
